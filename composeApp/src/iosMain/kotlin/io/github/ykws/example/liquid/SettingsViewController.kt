a953470b2f968185a7592b7f1c1a81cb245b1e0a
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2025-12-14 13:32:54 +0900

    iOS SettingsScreen 初期表示の上部余白を確保

--- composeApp/src/iosMain/kotlin/io/github/ykws/example/liquid/SettingsViewController.kt
+++ composeApp/src/iosMain/kotlin/io/github/ykws/example/liquid/SettingsViewController.kt
@@ -1,13 +1,14 @@
 package io.github.ykws.example.liquid
 
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 
 @Suppress("unused")
 fun settingsViewController() = ComposeUIViewController {
     MaterialTheme {
-        SettingsScreen(paddingValues = PaddingValues(0.dp))
+        SettingsScreen(paddingValues = WindowInsets.safeDrawing.asPaddingValues())
     }
 }
